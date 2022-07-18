03cd51d45719f58e607667e03bda8b48402d5b08
Deepak <bbaz2deepak@gmail.com>
2022-07-18 20:22:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useGetAllPostQuery, useGetPostByIdQuery } from './services/post'
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
+// import { useEffect } from 'react';
+
 
 function App() {
+  const responseInfo = useGetPostByIdQuery(5);
+  console.log(responseInfo)
+  console.log(responseInfo.data)
+  console.log(responseInfo.isSuccess)
+  // const responseInfo = useGetAllPostQuery();
+  // console.log(responseInfo.data);
+  // console.log(responseInfo.isSuccess);
+
+  //   if (responseInfo.isLoading) return <Stack sx={{ color: 'grey.500' }}
+
+  //     spacing={2} direction="row">
+  //     <CircularProgress color="success" />
+  //   </Stack>
+  //   if (responseInfo.isError) return <h1>Error Sorry Bro...</h1>
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Redux Toolkit</h1>
+    {
+      responseInfo.data.map((post) => (
+        <div key={post.id}>
+          <h2>Title: {post.title}</h2>
+          <h4>Body: {post.body}</h4>
+          <hr /> */}
+
+      <h1>{responseInfo.data} {responseInfo.data.title} </h1>
+      <h6>{responseInfo.body}</h6>
+
+
+
     </div>
   );
 }
---
+++ src/services/post.js
@@ -0,0 +1,25 @@
+import {  createApi, fetchBaseQuery  } from '@reduxjs/toolkit/query/react';
+
+export const postApi = createApi({
+    reducerPath: 'postApi',
+    baseQuery: fetchBaseQuery({baseUrl: 'http://jsonplaceholder.typicode.com/'}),
+    endpoints: (builder) => ({
+        getAllPost: builder.query({
+            query: () => ({
+                url: 'posts',
+                method: 'GET'
+            })
+        }),
+
+        getPostById: builder.query({
+            query: (id) => ({
+                url: `posts/${id}`,
+                method: 'GET'
+            })
+        })
+    })
+})
+
+
+
+export const { useGetAllPostQuery, useGetPostByIdQuery } = postApi;
